18aa0d91fa923c8177f478483dc9b012b7aed45f
Paul Ruvolo <paullundyruvolo@gmail.com>
2022-02-24 22:42:37 -0500

    Initial Commit

---
+++ TestMotion/TestMotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMotionApp.swift
+//  TestMotion
+//
+//  Created by Paul Ruvolo on 2/24/22.
+//
+
+import SwiftUI
+
+@main
+struct TestMotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
